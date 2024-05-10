5155d37e06e390c52c533c09833d08db70bd8bd4
GooseOb <gooose.ob@gmail.com>
2024-05-10 15:07:32 +0200

    add version pulling from package.json

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Building index.ts and header.txt into one js-file
+Building `index.ts` and `header.txt` into one js-file
+
+Replaces `{version}` in `header.txt` by `version` from `package.json`.
 
 # Usage
 
@@ -8,7 +10,9 @@ bunx bun-build-userscript
 # build
 bunx bun-build-userscript --build
 ```
+
 or
+
 ```bash
 bun add --global bun-build-userscript
 
@@ -21,22 +25,21 @@ bun build-userscript --build
 # Options
 
 ## --out
+
 Specify the output path
+
 ```bash
 bun build-userscript --out index.js
 ```
 
 ## --cfg
+
 Pass custom bun config
+
 ```bash
 bun build-userscript --cfg config.ts
 ```
+
 Should be exported by `export default`
 
 ## --log-errors
-Add error catching and printing to the console
-
-# Projects using this tool
-
-- [usos-dark](https://github.com/GooseOb/usos-dark)
-- [YT-Defaulter](https://github.com/GooseOb/YT-Defaulter)
--- index.ts
+++ index.ts
@@ -1,42 +1,48 @@
-import { readFile } from 'node:fs/promises';
-import type { BuildConfig } from 'bun';
+import { readFile } from "node:fs/promises";
+import * as path from "node:path";
+import type { BuildConfig } from "bun";
 
 export interface BunBuildUserscriptConfig extends BuildConfig {
-	userscript: {
-		logErrors: boolean
-	}
+  userscript: {
+    logErrors: boolean;
+  };
 }
 
 export const print = (msg: string) => {
-	process.stdout.write('\x1b[35m[bun-build-userscript]\x1b[0m ' + msg + '\n');
-}
+  process.stdout.write("\x1b[35m[bun-build-userscript]\x1b[0m " + msg + "\n");
+};
 
 const postprocess = (code: string) =>
-	'\n(function(){' +
-	code.replace(/(?:\\u[A-Fa-f\d]{4})+/g, ($0) => JSON.parse(`"${$0}"`)) +
-	'})()';
+  "\n(function(){" +
+  code.replace(/(?:\\u[A-Fa-f\d]{4})+/g, ($0) => JSON.parse(`"${$0}"`)) +
+  "})()";
 const addErrorLogging = (code: string) =>
-	`try{${code}}catch(e){console.log("[%cuserscript-error%c] %s","color: red","",e.toString())}`;
+  `try{${code}}catch(e){console.log("[%cuserscript-error%c] %s","color: red","",e.toString())}`;
 
 export const build = (config: Partial<BunBuildUserscriptConfig>) => {
-	const startTime = performance.now();
+  const startTime = performance.now();
 
-	return Bun.build({
-		entrypoints: ['index.ts'],
-		outdir: '.',
-		...config
-	}).then(async (output) => {
-		if (!output.success) {
-			print('error');
-			process.exit(1);
-		}
-		const {path} = output.outputs[0];
-		let result = postprocess(await readFile(path, 'utf-8'));
-		if (config.userscript?.logErrors) result = addErrorLogging(result);
-		await Bun.write(
-			path,
-			await readFile('header.txt', 'utf-8') + result
-		);
-		print(`done in ${performance.now() - startTime} ms`);
-	})
-}
+  return Bun.build({
+    entrypoints: ["index.ts"],
+    outdir: ".",
+    ...config,
+  }).then(async (output) => {
+    if (!output.success) {
+      print("error");
+      process.exit(1);
+    }
+    const { path: pathName } = output.outputs[0];
+    const postprocessHeader = async (text: string) => {
+      if (!text.includes("{version}")) return text;
+      const { version } = await import(path.resolve("package.json"));
+      return text.replace(/{version}/g, version);
+    };
+    let result = postprocess(await readFile(pathName, "utf-8"));
+    if (config.userscript?.logErrors) result = addErrorLogging(result);
+    await Bun.write(
+      pathName,
+      (await readFile("header.txt", "utf-8").then(postprocessHeader)) + result,
+    );
+    print(`done in ${performance.now() - startTime} ms`);
+  });
+};
